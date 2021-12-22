d121d0b145bd8d0ce44bc748dfd4138660731087
Mayya Mark <mayya.markova@gmail.com>
2021-12-22 12:06:42 +0200

    Refactoring

--- pages/api/mayya-data.js
+++ pages/api/mayya-data.js
@@ -1,22 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  const str = `To place a legal ad in the <NAME>, call <PHONE> with extension <EXTENSION> or send an email to <EMAIL>.\nOur notice specialists will be able to walk you through the process – but be prepared with:\nYour name, billing address, telephone number and email address\nThe text of the ad you wish to place\nAny images you must place in the ad (such as maps or logos)\nThe dates you’d like the ad to begin to run (subject to our deadlines). Some ads must run multiple times under state law; if so, let us know how many times (and on which dates) you want the ad to be published\nAll ads are automatically published on our website, <DOMAIN> and are automatically sent to the statewide public-notice site operated by the Florida Press Association`;
+  // Fetch the template...
+  const stringWithPlaceholders = `To place a legal ad in the {name}, call {phone} with extension {extension} or send an email to {email}.\nOur notice specialists will be able to walk you through the process – but be prepared with:\nYour name, billing address, telephone number and email address\nThe text of the ad you wish to place\nAny images you must place in the ad (such as maps or logos)\nThe dates you’d like the ad to begin to run (subject to our deadlines). Some ads must run multiple times under state law; if so, let us know how many times (and on which dates) you want the ad to be published\nAll ads are automatically published on our website, {domain} and are automatically sent to the statewide public-notice site operated by the Florida Press Association`;
   
+  // Fetch the replacements...
   const replacements = {
-    "<NAME>": 'App',
-    "<PHONE>": '088',
-    "<EXTENSION>": '3',
-    "<EMAIL>": 'test@gmail.com',
-    "<DOMAIN>": 'http://app.com/public-notices'
+    name: 'App',
+    phone: '088',
+    extension: '3',
+    email: 'test@gmail.com',
+    domain: 'http://app.com/public-notices'
   }
 
-  const formattedStr = str.replace(/<\w+>/g, function(all) {
-    return Object.prototype.hasOwnProperty.call(replacements, all) ? replacements[all] : all;
-  });
+  // Fill the template
+  const formattedStr = stringWithPlaceholders.replace(
+    /{(\w+)}/g, 
+    (placeholderWithDelimiters, placeholderWithoutDelimiters) =>
+    replacements.hasOwnProperty(placeholderWithoutDelimiters) ? 
+      replacements[placeholderWithoutDelimiters] : placeholderWithDelimiters
+  );
 
+  // Create an array of paragraphs
   const template = formattedStr.split("\n");
-  console.log(formattedStr, template)
 
   res.status(200).json({ template });
 }
--- pages/mayya/index.js
+++ pages/mayya/index.js
@@ -1,46 +1,13 @@
 import { useState, useEffect } from 'react';
 
-const obj = {
-  contact_us: {
-    text_template: "Test {testA}",
-    text_elements: {
-      phone_number: { 
-      type : "number",
-      value : "088"
-    },
-  }
-}}
-
-const str = `To place a legal ad in the <NAME>, call <PHONE> with extension <EXTENSION> or send an email to <EMAIL>. 
-Our notice specialists will be able to walk you through the process – but be prepared with:
-&#8226; Your name, billing address, telephone number and email address
-&#8226; The text of the ad you wish to place
-&#8226; Any images you must place in the ad (such as maps or logos)
-&#8226; The dates you’d like the ad to begin to run (subject to our deadlines). Some ads must run multiple times under state law; if so, let us know how many times (and on which dates) you want the ad to be published
-All ads are automatically published on our website, <DOMAIN> and are automatically sent to the statewide public-notice site operated by the Florida Press Association`;
-
-const replacements = {
-  NAME: 'App',
-  PHONE: '088',
-  EXTENSION: '3',
-  EMAIL: 'test@gmail.com',
-  DOMAIN: 'http://app.com/public-notices'
-}
-
 function Mayya() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState([]); // You can add a state for loading & error
 
   useEffect(() => {
-    (async () => {
-      const response = await fetch("/api/mayya-data");
-
-      if (!response.ok) {
-        console.log(`Error: ${response.status}`);
-      }
-    const data = await response.json();
-    console.log(data.formatted)
-    setData(data.template);
-    })();
+    fetch("/api/mayya-data")
+      .then((res) => res.json())
+      .then((json) => setData(json.template))
+      .catch((err) => console.log(err))
   }, []);
 
   return data.length > 0 
